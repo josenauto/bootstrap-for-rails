2d3894d504b4c2c51214fe934584ed2cb7d87bde
josenauto <jemilionautoch@gmail.com>
2015-09-27 15:52:04 -0300

    Add the migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150927184646) do
+ActiveRecord::Schema.define(version: 20150927184921) do
 
   create_table "todos", force: true do |t|
     t.string   "title"
